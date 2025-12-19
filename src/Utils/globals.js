539a7a1a1d2ee77593e132a0ed4bea36c6783437
SICMA ET ASSOCIES <dev@sicmagroup.com>
2025-12-19 12:08:03 +0100

    changement url rapport excel apres activation ssl

--- src/Utils/globals.js
+++ src/Utils/globals.js
@@ -23,9 +23,8 @@ function hostEval(){
 }
 function reportHost(){
     // return "http://127.0.0.1:5000/"
-    // return "https://gpr-rapport-excel-python-script.onrender.com/"
     // return "https://api.rapport.gprserver.com:8081/"
-    return "http://api.rapport.gprserver.com/"
+    return "https://api.rapport.gprserver.com/"
 }
 
 export const LOCKSCREEN_TIMEOUT = 300000; //5min
