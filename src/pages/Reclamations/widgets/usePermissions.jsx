bc723330145fe03155591bf33905f23eb452cb2a
SICMA ET ASSOCIES <dev@sicmagroup.com>
2026-01-15 11:43:27 +0100

    revue des permissions pour ouverture de session

--- src/pages/Reclamations/widgets/usePermissions.jsx
+++ src/pages/Reclamations/widgets/usePermissions.jsx
@@ -9,11 +9,15 @@ export const usePermissions = (user, props) => {
       props.transmitted !== "false" &&
       user.firstAndLastName === props.transmittedBy;
 
-    const isAffecter =
-      user.firstAndLastName === props.assigned_by;
-
+    const isAffecter = user.firstAndLastName === props.assigned_by;
+    const isAffectedUser = user.firstAndLastName === props.handled_by;
+    const isAuthorButNotAffected = isAuthor && !isAffectedUser;
+    // la réclamation est affectée à quelqu’un d’autre ?
+    const isAffectedToSomeoneElse = props.handled_by && !isAffectedUser;
+    const isAuthorAndIsAffected = isAuthor && isAffectedUser;
+   
     const isAuthorWithAuthorization =
-      isAuthor && hasAuthorization && !isTransmitter && !isAffecter;
+      isAuthor && hasAuthorization && !isTransmitter && !isAffecter && !isAffectedToSomeoneElse;
 
     const isTransmittedToUser =
       props.transmitted !== "false" &&
@@ -21,13 +25,13 @@ export const usePermissions = (user, props) => {
       props.status === "SAVED" &&
       props.addR === "MOLDUE";
 
-    const isAffectedUser = user.firstAndLastName === props.handled_by;
+  
 
     const isUserOpenSession =
       props.session && user.firstAndLastName === props.session?.createdBy?.firstAndLastName;
 
     const isRa = user.ra === true;
-    const raCanOpenSession = (isRa && props.unit === user.servicePointDto.libelle) && !isTransmitter && !isAffecter;
+    const raCanOpenSession = (isRa && props.unit === user.servicePointDto.libelle) && !isTransmitter && !isAffecter && !isAffectedToSomeoneElse;
    
     return {
       isAuthor,
